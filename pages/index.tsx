17c8e35fa1e0530201cae090af5c49c36ad1324f
Taibu Haroub <haroubtaibu@gmail.com>
2022-05-10 01:38:27 +0300

    Add sidebar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Twitter Clone</title>
       </Head>
 
-      <h1>Twitter Clone</h1>
+      <main>
+        {/* Sidebar  */}
+        <Sidebar />
+
+        {/* Feed  */}
+
+        {/* Widgets  */}
+      </main>
 
       
     </div>
